d5d465fa91e61815e934c6ba445edbbe04d80418
sonnt0309 <sonnt0309@gmail.com>
2019-07-19 10:08:56 +0700

    custom abstract user

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class User(AbstractUser):
+    created_date = models.DateTimeField(default=timezone.now, verbose_name=_('created date'))
+    updated_date = models.DateTimeField(default=timezone.now, verbose_name=_('updated date'))
+    is_deleted = models.BooleanField(default=False, verbose_name=_('is deleted'))
--- plantsmart/settings.py
+++ plantsmart/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app
+    'apps.users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
